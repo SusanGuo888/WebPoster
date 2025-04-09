71debaa5442e505101d6923ded19109a1505a819
SusanGuo888 <bajeooo@icloud.com>
2025-04-08 20:34:59 -0400

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,56 @@
+//alert("Javascript Linked")
+//console.log("Javascript Linked")
+
+$("#title").hover(function(){
+    $("#title").css("font-size","70")
+},
+function(){
+    $("#title").css("font-size","5")
+})
+
+$("#title").hover(function(){
+    $("#title").css("height","200")
+},
+function(){
+    $("#title").css("height","20")
+})
+
+$("#title").hover(function(){
+    $("#title").css("width","1000")
+},
+function(){
+    $("#title").css("width","100")
+})
+
+$( function() {
+    $( "#resizable" ).resizable();
+  } )
+
+
+  $("#intro").hover(function(){
+    $("#intro").css("font-size","20")
+},
+function(){
+    $("#intro").css("font-size","5")
+})
+
+$("#intro").hover(function(){
+    $("#intro").css("height","100")
+},
+function(){
+    $("#intro").css("height","20")
+})
+
+$("#intro").hover(function(){
+    $("#intro").css("width","1000")
+},
+function(){
+    $("#intro").css("width","100")
+})  
+
+$( function() {
+    $( "#resizable-2" ).resizable();
+  } )
+
+
+
